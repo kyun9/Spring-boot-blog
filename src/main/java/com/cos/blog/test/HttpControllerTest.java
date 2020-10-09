8dbdd699fe972d6c3b3f79c83461d70b4a5ac9b8
kyun9 <gusl108@naver.com>
2020-10-09 14:29:46 +0900

    201009

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 // 사용자가 요청 --> 응답(HTML 파일)
@@ -16,14 +17,15 @@ public class HttpControllerTest {
 	//인터넷 브라우저 요청은 무조건 get요청밖에 할 수 없다.
 	// http://localhost:8080/http/get (select)
 	@GetMapping("/http/get")
-	public String getTest() {
-		return "get 요청";
+	public String getTest(Member m) {
+		return "get 요청: "+ m.getId()+" "+ m.getUsername()+" "+ m.getPassword()+ " "+ m.getEmail();
 	}
 
 	// http://localhost:8080/http/post (insert)
 	@PostMapping("/http/post")
-	public String postTest() {
-		return "post 요청";
+	public String postTest(@RequestBody Member m) {
+//		return "post 요청 : "+text;
+		return "post 요청: "+ m.getId()+" "+ m.getUsername()+" "+ m.getPassword()+ " "+ m.getEmail();
 	}
 
 	// http://localhost:8080/http/put  (update)
