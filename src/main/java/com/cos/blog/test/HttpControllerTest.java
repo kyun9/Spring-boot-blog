665bd5f52e9627c5f36e8fed1f2adcbcecbf383f
kyun9 <gusl108@naver.com>
2021-02-01 23:19:16 +0900

    lombok 설정

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -13,22 +13,36 @@ import org.springframework.web.bind.annotation.RestController;
 // 사용자가 요청 --> 응답(Data)
 @RestController
 public class HttpControllerTest {
-	
-	//인터넷 브라우저 요청은 무조건 get요청밖에 할 수 없다.
+
+	private static final String TAG = "HttpControllerTest : ";
+
+	@GetMapping("/http/lombok")
+	public String lombokTest() {
+//		Member m = new Member(1, "ssar", "1234", "email");
+		Member m = Member.builder().username("ssar").password("1234").email("ssar@nate.com").build();
+		System.out.println(TAG + "getter : " + m.getUsername());
+		m.setUsername("cos");
+		System.out.println(TAG + "getter : " + m.getUsername());
+
+		return "lombok test완료";
+	}
+
+	// 인터넷 브라우저 요청은 무조건 get요청밖에 할 수 없다.
 	// http://localhost:8080/http/get (select)
 	@GetMapping("/http/get")
 	public String getTest(Member m) {
-		return "get 요청: "+ m.getId()+" "+ m.getUsername()+" "+ m.getPassword()+ " "+ m.getEmail();
+
+		return "get 요청: " + m.getId() + " " + m.getUsername() + " " + m.getPassword() + " " + m.getEmail();
 	}
 
 	// http://localhost:8080/http/post (insert)
 	@PostMapping("/http/post")
 	public String postTest(@RequestBody Member m) {
 //		return "post 요청 : "+text;
-		return "post 요청: "+ m.getId()+" "+ m.getUsername()+" "+ m.getPassword()+ " "+ m.getEmail();
+		return "post 요청: " + m.getId() + " " + m.getUsername() + " " + m.getPassword() + " " + m.getEmail();
 	}
 
-	// http://localhost:8080/http/put  (update)
+	// http://localhost:8080/http/put (update)
 	@PutMapping("/http/put")
 	public String putTest() {
 		return "put 요청";
